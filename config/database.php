8242eaa24945c83057362cb224cffaf28031153f
Antonis Antoniou <kountouris7@gmail.com>
2018-09-02 15:52:46 +0300

    update database

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql_production' ),
+    'default' => env('DB_CONNECTION', 'mysql' ),
 
     /*
     |--------------------------------------------------------------------------
@@ -39,22 +39,22 @@ return [
             'prefix' => '',
         ],
 
-        'mysql' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-        ],
+        //'mysql' => [
+      //    'driver' => 'mysql',
+      //    'host' => env('DB_HOST', '127.0.0.1'),
+      //    'port' => env('DB_PORT', '3306'),
+      //    'database' => env('DB_DATABASE', 'forge'),
+      //    'username' => env('DB_USERNAME', 'forge'),
+      //    'password' => env('DB_PASSWORD', ''),
+      //    'unix_socket' => env('DB_SOCKET', ''),
+      //    'charset' => 'utf8mb4',
+      //    'collation' => 'utf8mb4_unicode_ci',
+      //    'prefix' => '',
+      //    'strict' => true,
+      //    'engine' => null,
+      //],
 
-        'mysql_production' => [
+        'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
