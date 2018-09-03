9e6e0a1309bb93b3879776ce4c59d00df13dfb36
Antonis Antoniou <kountouris7@gmail.com>
2018-09-03 13:46:45 +0300

    host array empty

--- config/database.php
+++ config/database.php
@@ -56,8 +56,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-
-            'host' => env('DB_HOST'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT'),
             'database' => env('DB_DATABASE'),
             'username' => env('DB_USERNAME'),
