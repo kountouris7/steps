cce4672d9a19c2cc0e4952473e7cb25c8f069aca
Antonis Antoniou <kountouris7@gmail.com>
2018-09-03 12:18:45 +0300

    database

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
-$DATABASE_URL = parse_url(getenv("DATABASE_URL"));
+
 return [
 
     /*
@@ -57,11 +57,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
 
-            'host' => $DATABASE_URL["host"],
-            'port' => $DATABASE_URL["port"],
-            'database' => ltrim($DATABASE_URL["path"], "/"),
-            'username' => $DATABASE_URL["user"],
-            'password' => $DATABASE_URL["pass"],
+            'host' => env('DB_HOST'),
+            'port' => env('DB_PORT'),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
             'unix_socket' => env('DB_SOCKET'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
