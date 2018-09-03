79d13a8435f4f2029bf8a008f938f0ee16c999c1
Antonis Antoniou <kountouris7@gmail.com>
2018-09-03 13:48:58 +0300

    again

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT'),
             'database' => env('DB_DATABASE'),
             'username' => env('DB_USERNAME'),
