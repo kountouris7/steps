0c4c69ca15422e0338cdd8625fc1f41ee13a6665
Antonis Antoniou <kountouris7@gmail.com>
2018-09-03 11:03:38 +0300

    .env rev4

--- config/database.php
+++ config/database.php
@@ -56,12 +56,13 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => 'db4free.net',
-            'port' => '3306',
-            'database' => 'stepsfitness',
-            'username' => 'kountouris',
-            'password' => '00711855a',
-           // 'unix_socket' => env('DB_SOCKET', ''),
+
+            'host' => env('DB_HOST'),
+            'port' => env('DB_PORT'),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+            'unix_socket' => env('DB_SOCKET'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
