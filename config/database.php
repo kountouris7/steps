77c22731b76f6517d6526bf8e7afba0e9857623d
Antonis Antoniou <kountouris7@gmail.com>
2018-09-03 12:14:02 +0300

    database

--- config/database.php
+++ config/database.php
@@ -1,5 +1,14 @@
 <?php
+$db = parse_url(getenv("DATABASE_URL"));
 
+$pdo = new PDO("mysql:" . sprintf(
+        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+        $db["host"],
+        $db["port"],
+        $db["user"],
+        $db["pass"],
+        ltrim($db["path"], "/")
+    ));
 return [
 
     /*
