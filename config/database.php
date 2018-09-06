5970e7605ce5ecba4f992759ab540fafe20cf864
Antonis Antoniou <kountouris7@gmail.com>
2018-09-06 13:01:31 +0300

    database 1st com

--- config/database.php
+++ config/database.php
@@ -13,7 +13,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql_prod' ),
+    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -40,36 +41,21 @@ return [
         ],
 
         'mysql' => [
-          'driver' => 'mysql',
-          'host' => env('DB_HOST', '127.0.0.1'),
-          'port' => env('DB_PORT', '3306'),
-          'database' => env('DB_DATABASE', 'forge'),
-          'username' => env('DB_USERNAME', 'forge'),
-          'password' => env('DB_PASSWORD', ''),
-          'unix_socket' => env('DB_SOCKET', ''),
-          'charset' => 'utf8mb4',
-          'collation' => 'utf8mb4_unicode_ci',
-          'prefix' => '',
-          'strict' => true,
-          'engine' => null,
-      ],
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
 
-       'mysql_prod' => [
-           'driver' => 'mysql',
-           'host' => env('DB_HOST' , 'db4free.net'),
-           'port' => env('DB_PORT' , '3306'),
-           'database' => env('DB_DATABASE', 'stepsfitness'),
-           'username' => env('DB_USERNAME', 'kountouris'),
-           'password' => env('DB_PASSWORD' ,'00711855a'),
-           'unix_socket' => env('DB_SOCKET' , ''),
-           'charset' => 'utf8mb4',
-           'collation' => 'utf8mb4_unicode_ci',
-           'prefix' => '',
-           'strict' => false,
-           'engine' => null,
-       ],
-
-        'pgsql_production' => [
+        'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
