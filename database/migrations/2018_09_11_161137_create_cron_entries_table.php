158312ebe72278948eb49493ec9cb52148f80df5
Antonis Antoniou <kountouris7@gmail.com>
2018-09-11 16:14:06 +0300

    cron table

---
+++ database/migrations/2018_09_11_161137_create_cron_entries_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCronEntriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cron_entries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('command');
+            $table->integer('next_run');
+            $table->integer('last_run');
+            $table->timestamps();
+            $table->primary('command');
+            $table->index('next_run');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cron_entries');
+    }
+}
