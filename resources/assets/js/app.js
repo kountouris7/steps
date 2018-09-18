add3463b979c92b3f74cf1887916f9edb10e02a1
Antonis Antoniou <kountouris7@gmail.com>
2018-09-18 11:44:56 +0300

    vue5

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -16,6 +16,7 @@ window.Vue = require('vue');
  */
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('show-groups', require('./ShowGroups.vue'));
 
 const app = new Vue({
     el: '#app'
