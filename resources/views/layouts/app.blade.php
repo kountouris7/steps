14e1e6eadf26d2e6f63c0a6810cab68487ff4b65
Antonis Antoniou <kountouris7@gmail.com>
2018-10-05 23:12:08 +0300

    removes comments

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,8 +15,6 @@
     </script>
 </head>
 <body>
-{{--<script src="https://unpkg.com/axios/dist/axios.min.js"></script>--}}
-{{--<script src="https://unpkg.com/vue@2.5.17/dist/vue.js"></script>--}}{{--u should change this to the production url--}}
 
 <div id="app">
     @include('layouts.nav')
@@ -26,11 +24,6 @@
 </div>
 
 <script src="https://js.pusher.com/4.3/pusher.min.js"></script>
-{{--<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>--}}
-<!-- Compiled and minified JavaScript -->
-{{--<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>--}}
-<!-- this is for dropdown btn -->
-<script></script>
 
 <!--JavaScript at end of body for optimized loading-->
 <script src="{{asset('js/app.js')}}"></script>
@@ -38,7 +31,7 @@
 <script>
     $(document).ready(function () {
         $('.collapsible').collapsible();
-        $(".dropdown-trigger").dropdown();
+        $('.dropdown-trigger').dropdown();
         $('.parallax').parallax();
         $('.carousel.carousel-slider').carousel({
             fullWidth: true,
@@ -49,9 +42,6 @@
         $('.materialboxed').materialbox();
     });
 </script>
-
-{{--<script type="text/javascript" src="{{asset('js/materialize.min.js')}}"></script>--}}
-
 
 </body>
 </html>
