de81a99f8d12ad6216420e0e92df390b5388fabc
Antonis Antoniou <kountouris7@gmail.com>
2018-10-05 23:40:20 +0300

    Changes groups booking message

--- resources/assets/js/components/Groups.vue
+++ resources/assets/js/components/Groups.vue
@@ -1,11 +1,10 @@
 <template>
     <button type="submit" :class="classes" class="waves-effect pink accent-3 btn-small" @click="toggle">
-        <span v-text="bookingsCount"></span>
+        <span v-text="getBookButtonText"></span>
     </button>
 </template>
 
 <script>
-
     export default {
 
         props: ['group'],
@@ -14,6 +13,7 @@
             return {
                 bookingsCount: this.group.bookingsCount,
                 isBooked: this.group.isBooked,
+                maxCapacity: this.group.max_capacity,
                 //clients: this.group.clients
             }
         },
@@ -21,28 +21,29 @@
         computed: {
             classes() {
                 return [this.isBooked ? 'disabled' : ''];
+            },
+            getBookButtonText() {
+                return this.bookingsCount + ' of ' + this.maxCapacity + ' available.';
             }
-
         },
 
         methods: {
             toggle() {
                 axios.post('/booking/' + this.group.id)
                     .then(response => {
+                        this.isBooked = true;
+                        this.bookingsCount++;
+
                         console.log(response)
                     })
                     .catch(error => {
+                        alert('kolos')
                         console.log(error.response)
                     });
 
-                this.isBooked = true;
-                this.bookingsCount++;
 
             },
-
-
         }
-
     }
 </script>
 
