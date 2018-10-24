bc13fa1a1c4e270e4b9dba2d5b19010fb4b45302
Antonis Antoniou <kountouris7@gmail.com>
2018-10-24 16:20:56 +0300

    smartphones UI fixes 2

--- resources/views/filterdays.blade.php
+++ resources/views/filterdays.blade.php
@@ -1,6 +1,7 @@
 <nav class="nav-extended">
     <div class="nav-wrapper black">
-        <ul class="left-align col s12 m4 s6">
+        <a href="#" data-target="filter-days" class="sidenav-trigger"><i class="material-icons">today</i></a>
+        <ul class="left-align hide-on-med-and-down">
             <li><a href="{{route('groups.by.day', [0])}}">Monday</a></li>
             <li><a><a href="{{route('groups.by.day', [1])}}">Tuesday</a></a></li>
             <li><a href="{{route('groups.by.day', [2])}}">Wednesday</a></li>
@@ -9,3 +10,11 @@
         </ul>
     </div>
 </nav>
+
+<ul class="sidenav" id="filter-days">
+    <li><a href="{{route('groups.by.day', [0])}}">Monday</a></li>
+    <li><a><a href="{{route('groups.by.day', [1])}}">Tuesday</a></a></li>
+    <li><a href="{{route('groups.by.day', [2])}}">Wednesday</a></li>
+    <li><a href="{{route('groups.by.day', [3])}}">Thursday</a></li>
+    <li><a href="{{route('groups.by.day', [4])}}">Friday</a></li>
+</ul>
