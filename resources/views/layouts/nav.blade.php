4e33e088b9c97bf3a681d2f968112f408010d9d2
Antonis Antoniou <kountouris7@gmail.com>
2018-10-24 14:46:35 +0300

    screen size reactivity -- navbar -- fixed

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -46,25 +46,36 @@
     <li><a href="{{route('home')}}">Home</a></li>
     <li><a href="{{route('show.groups')}}">Classes</a></li>
 
-    <li><a href="{{ route('login') }}">Login</a></li>
-    <li><a href="{{ route('register.user') }}">Register</a></li>
-    <li><a href="{{ route('profile.dashboard', Auth::user()) }}">My Profile</a></li>
-    <li><a href="{{ route('logout') }}"
-           onclick="event.preventDefault();
-                                             document.getElementById('logout-form').submit();">
-            Logout</a></li>
-    <li class="divider" tabindex="-1"></li>
+    @if (Auth::guest())
+        <li><a href="{{ route('login') }}">Login</a></li>
+        <li><a href="{{ route('register.user') }}">Register</a></li>
+@else
 
-    @if(Auth::user()->isAdmin())
-        <li><a href="{{ route('admin') }}">Admin Panel</a></li>
-    @endif
+        <ul>
+            <li><a href="{{ route('profile.dashboard', Auth::user()) }}">My Profile</a></li>
+            <li class="divider" tabindex="-1"></li>
+
+            @if(Auth::user()->isAdmin())
+                <li><a href="{{ route('admin') }}">Admin Panel</a></li>
+
+                <li class="divider" tabindex="-1"></li>
+            @endif
 
 
-    <form id="logout-form" action="{{ route('logout') }}" method="POST"
-          style="display: none;">
-        {{ csrf_field() }}
-    </form>
+            <li><a href="{{ route('logout') }}"
+                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                    Logout</a></li>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                  style="display: none;">
+                {{ csrf_field() }}
+            </form>
+            @endif
 </ul>
+</ul>
+
+
 
 
 
