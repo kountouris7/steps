0cf01cf58fe7b94518f4b4311b08a987e6ff7b07
Antonis Antoniou <kountouris7@gmail.com>
2018-12-02 16:47:41 +0200

    image display fixed google drive foder public access

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,19 @@
     <div class="carousel carousel-slider center" xmlns:v-on="http://www.w3.org/1999/xhtml"
          xmlns:v-on="http://www.w3.org/1999/xhtml" xmlns:v-on="http://www.w3.org/1999/xhtml"
          xmlns:v-on="http://www.w3.org/1999/xhtml">
+
         <div class="carousel-fixed-item center">
             {{--<a class="btn waves-effect white grey-text darken-text-2">button</a>--}}
         </div>
         <div class="carousel-item white black-text" href="#one!" style="height: 315px">
             <img src="http://drive.google.com/uc?export=view&id=1aj6WYC8bzVaL2jX3kMj9ppiiB-yMP7zi"
                  href="#one!"
+                 style="width: 100%; height: 315px;">
+        </div>
+
+        <div class="carousel-item white black-text" href="#two!" style="height: 315px">
+            <img src="http://drive.google.com/uc?export=view&id=1aj6WYC8bzVaL2jX3kMj9ppiiB-yMP7zi"
+                 href="#two!"
                  style="width: 100%; height: 315px;">
         </div>
     </div>
